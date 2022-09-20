34554204eac3073b88f3b031013d8f025a730855
Александр <zalaw@bk.ru>
2022-09-20 15:23:19 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace TheMeasurer
+{
+    partial class FormMeasurer
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMeasurer));
+            this.notifyIconTray = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStripTray = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.режимПеретаскиванияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.координатыНачалаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.непрозрачночтьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.выйтиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.PB = new System.Windows.Forms.PictureBox();
+            this.contextMenuStripTray.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIconTray
+            // 
+            this.notifyIconTray.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIconTray.Icon")));
+            this.notifyIconTray.Text = "notifyIconTray";
+            this.notifyIconTray.Visible = true;
+            // 
+            // contextMenuStripTray
+            // 
+            this.contextMenuStripTray.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.режимПеретаскиванияToolStripMenuItem,
+            this.координатыНачалаToolStripMenuItem,
+            this.непрозрачночтьToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.выйтиToolStripMenuItem});
+            this.contextMenuStripTray.Name = "contextMenuStripTray";
+            this.contextMenuStripTray.Size = new System.Drawing.Size(204, 98);
+            // 
+            // режимПеретаскиванияToolStripMenuItem
+            // 
+            this.режимПеретаскиванияToolStripMenuItem.CheckOnClick = true;
+            this.режимПеретаскиванияToolStripMenuItem.Name = "режимПеретаскиванияToolStripMenuItem";
+            this.режимПеретаскиванияToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.режимПеретаскиванияToolStripMenuItem.Text = "Режим перетаскивания";
+            this.режимПеретаскиванияToolStripMenuItem.Click += new System.EventHandler(this.режимПеретаскиванияToolStripMenuItem_Click);
+            // 
+            // координатыНачалаToolStripMenuItem
+            // 
+            this.координатыНачалаToolStripMenuItem.CheckOnClick = true;
+            this.координатыНачалаToolStripMenuItem.Name = "координатыНачалаToolStripMenuItem";
+            this.координатыНачалаToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.координатыНачалаToolStripMenuItem.Text = "Координаты начала";
+            this.координатыНачалаToolStripMenuItem.Click += new System.EventHandler(this.координатыНачалаToolStripMenuItem_Click);
+            // 
+            // непрозрачночтьToolStripMenuItem
+            // 
+            this.непрозрачночтьToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem5});
+            this.непрозрачночтьToolStripMenuItem.Name = "непрозрачночтьToolStripMenuItem";
+            this.непрозрачночтьToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.непрозрачночтьToolStripMenuItem.Text = "Непрозрачночть";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(89, 22);
+            this.toolStripMenuItem2.Text = "0,1";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.Opacity_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(89, 22);
+            this.toolStripMenuItem3.Text = "0,3";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.Opacity_Click);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(89, 22);
+            this.toolStripMenuItem4.Text = "0,5";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.Opacity_Click);
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(89, 22);
+            this.toolStripMenuItem5.Text = "0,8";
+            this.toolStripMenuItem5.Click += new System.EventHandler(this.Opacity_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(200, 6);
+            // 
+            // выйтиToolStripMenuItem
+            // 
+            this.выйтиToolStripMenuItem.Name = "выйтиToolStripMenuItem";
+            this.выйтиToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.выйтиToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.выйтиToolStripMenuItem.Text = "Выйти";
+            this.выйтиToolStripMenuItem.Click += new System.EventHandler(this.выйтиToolStripMenuItem_Click);
+            // 
+            // PB
+            // 
+            this.PB.Cursor = System.Windows.Forms.Cursors.Default;
+            this.PB.Location = new System.Drawing.Point(0, 0);
+            this.PB.Name = "PB";
+            this.PB.Size = new System.Drawing.Size(374, 273);
+            this.PB.TabIndex = 1;
+            this.PB.TabStop = false;
+            this.PB.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PB_MouseClick);
+            this.PB.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FormMeasurer_MouseDown);
+            this.PB.MouseMove += new System.Windows.Forms.MouseEventHandler(this.FormMeasurer_MouseMove);
+            this.PB.MouseUp += new System.Windows.Forms.MouseEventHandler(this.FormMeasurer_MouseUp);
+            // 
+            // FormMeasurer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(713, 507);
+            this.Controls.Add(this.PB);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMeasurer";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TheMeasurer";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FormMeasurer_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormMeasurer_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FormMeasurer_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.FormMeasurer_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.FormMeasurer_MouseUp);
+            this.contextMenuStripTray.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PB)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIconTray;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripTray;
+        private System.Windows.Forms.ToolStripMenuItem выйтиToolStripMenuItem;
+        private System.Windows.Forms.PictureBox PB;
+        private System.Windows.Forms.ToolStripMenuItem непрозрачночтьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem режимПеретаскиванияToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem координатыНачалаToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TheMeasurer
+{
+    public partial class FormMeasurer : Form
+    {
+        int XStart, YStart, rectWidth, rectHeight;
+        private Point pointStart;
+        private Point rectStart;
+        private bool movingMode = false;
+        private bool dragNow = false;
+        private const int CORECTPOS = 8; // смещение точки рисования под угол курсора
+
+        public FormMeasurer()
+        {
+            InitializeComponent();
+            notifyIconTray.MouseClick += new MouseEventHandler(notifyIcon_MouseClick);
+            this.ShowInTaskbar = false;
+            this.WindowState = FormWindowState.Maximized;
+            this.TopMost = true;
+        }
+
+        private void FormMeasurer_Load(object sender, EventArgs e)
+        {
+            this.Opacity = 0.3;
+            this.FormBorderStyle = FormBorderStyle.None;
+            PB.Height = this.Height;
+            PB.Width = this.Width;
+            //this.AllowTransparency = true;
+            //this.BackColor = Color.AliceBlue;//цвет фона  
+            //this.TransparencyKey = this.BackColor;//он же будет заменен на прозрачный цвет
+        }
+
+        private void PB_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                PB.ContextMenuStrip = contextMenuStripTray;
+            }
+        }
+        private void FormMeasurer_MouseDown(object sender, MouseEventArgs e)
+        {
+            pointStart = new Point(MousePosition.X, MousePosition.Y);
+            var pic = (PictureBox)sender;
+            if (movingMode)
+            {
+                rectStart = new Point(XStart, YStart);
+                if (PointInRectangle()) dragNow = true;
+            }
+        }
+
+        private void FormMeasurer_MouseUp(object sender, MouseEventArgs e)
+        {
+            pointStart = Point.Empty;
+            dragNow = false;
+        }
+
+        private bool PointInRectangle()
+        {
+            return (MousePosition.X > XStart && MousePosition.Y > YStart && MousePosition.X < (XStart + rectWidth) && MousePosition.Y < (YStart + rectHeight));
+        }
+
+        private void FormMeasurer_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right) return;
+            if (pointStart == Point.Empty) return;
+
+            PointCalculation();
+            DrawRectangle(sender);
+        }
+
+        private void PointCalculation()
+        {
+            if (dragNow)
+            {
+                //MessageBox.Show("XStart=" + XStart + "\n=MousePosition.X" + MousePosition.X + "\npointStart.X" + pointStart.X);
+                XStart = rectStart.X + MousePosition.X - pointStart.X;
+                YStart = rectStart.Y + MousePosition.Y - pointStart.Y;
+            }
+            else
+            {
+                if (pointStart.X < MousePosition.X)
+                {
+                    XStart = pointStart.X;
+                    rectWidth = MousePosition.X - XStart;
+                }
+                else
+                {
+                    XStart = MousePosition.X;
+                    rectWidth = pointStart.X - XStart;
+                }
+
+                if (pointStart.Y < MousePosition.Y)
+                {
+                    YStart = pointStart.Y;
+                    rectHeight = MousePosition.Y - YStart;
+                }
+                else
+                {
+                    YStart = MousePosition.Y;
+                    rectHeight = pointStart.Y - YStart;
+                }
+            }
+        }
+
+        private void DrawRectangle(object sender)
+        {
+            var pic = (PictureBox)sender;
+            if (pic.Image == null) { pic.Image = new Bitmap(pic.Width, pic.Height); }
+            SolidBrush myBrush = new SolidBrush(Color.Red);
+            using (Graphics g = pic.CreateGraphics())
+            {
+                pic.Refresh();
+                var rect = new Rectangle(XStart + CORECTPOS, YStart + CORECTPOS, rectWidth, rectHeight);
+                g.FillRectangle(myBrush, rect);
+
+
+                var fontColor = Brushes.LightGoldenrodYellow;
+                Point pointTextStart = new Point(XStart + CORECTPOS, YStart + CORECTPOS);
+                var checkWidth = координатыНачалаToolStripMenuItem.Checked ? 200 : 100;
+                if (rect.Height < 25 || rect.Width < checkWidth)
+                {
+                    pointTextStart.Y = YStart - 20;
+                    fontColor = Brushes.Black;
+                }
+
+                var str = rect.Height + "X" + rect.Width;
+                if (координатыНачалаToolStripMenuItem.Checked) str += " (" + YStart  + "x" + XStart + ")";
+
+                using (Font myFont = new Font("Arial", 14, FontStyle.Bold))
+                {
+                    g.DrawString(str, myFont, fontColor, new RectangleF(pointTextStart, new SizeF(200, 30)));// XStart + XEnd, YStart + YEnd);
+                    g.DrawString(str, myFont, Brushes.Black, new RectangleF(10, this.Height - 30, 200, 30));
+                    g.DrawString(str, myFont, Brushes.Black, new RectangleF(this.Width - 200, this.Height - 30, 200, 30));
+                    g.DrawString(str, myFont, Brushes.Black, new RectangleF(this.Width - 200, 10, 200, 30));
+                    g.DrawString(str, myFont, Brushes.Black, new RectangleF(10, 10, 200, 30));
+                }
+            }
+        }
+
+        private void Opacity_Click(object sender, EventArgs e)
+        {
+            this.Opacity = Convert.ToDouble(((ToolStripMenuItem)sender).Text);
+        }
+
+        private void режимПеретаскиванияToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (режимПеретаскиванияToolStripMenuItem.Checked)
+                movingMode = true;
+            else
+                movingMode = false;
+        }
+
+
+        #region TRAY
+        private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                notifyIconTray.ContextMenuStrip = contextMenuStripTray;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Maximized;
+                //this.Visible = true;
+            }
+        }
+
+
+        private void координатыНачалаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DrawRectangle(this.PB);
+        }
+
+        private void выйтиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        #endregion
+
+        private void FormMeasurer_KeyDown(object sender, KeyEventArgs e)
+        {
+            //if (e.KeyCode == Keys.Escape) this.Close();
+            //if (e.KeyCode == Keys.Escape) this.WindowState = FormWindowState.Minimized;
+
+            if      (e.KeyCode == Keys.Up && e.Modifiers == Keys.Shift) {if (rectHeight > 0) rectHeight--;}
+            else if (e.KeyCode == Keys.Down && e.Modifiers == Keys.Shift) rectHeight++;
+            else if (e.KeyCode == Keys.Left && e.Modifiers == Keys.Shift) { if (rectWidth > 0) rectWidth--;}
+            else if (e.KeyCode == Keys.Right && e.Modifiers == Keys.Shift) rectWidth++;
+            else if (e.KeyCode == Keys.Up && e.Shift && e.Control) {if (rectHeight > 0) rectHeight -= 10;}
+            else if (e.KeyCode == Keys.Down && e.Shift && e.Control) rectHeight += 10;
+            else if (e.KeyCode == Keys.Left && e.Shift && e.Control) {if (rectWidth > 0) rectWidth -= 10;}
+            else if (e.KeyCode == Keys.Right && e.Shift && e.Control) rectWidth += 10;
+            else if (e.KeyCode == Keys.Up && e.Control) rectStep(0, -10);
+            else if (e.KeyCode == Keys.Down && e.Control) rectStep(0, 10);
+            else if (e.KeyCode == Keys.Left && e.Control) rectStep(-10, 0);
+            else if (e.KeyCode == Keys.Right && e.Control) rectStep(10, 0);
+            else if (e.KeyCode == Keys.Up) rectStep(0, -1);
+            else if (e.KeyCode == Keys.Down) rectStep(0, 1);
+            else if (e.KeyCode == Keys.Left) rectStep(-1, 0);
+            else if (e.KeyCode == Keys.Right) rectStep(1, 0);
+            else return;
+
+            //DrawRectangle(((FormMeasurer)sender).PB);
+            DrawRectangle(this.PB);
+        }
+
+        private void rectStep(int XStep, int YStep)
+        {
+            if (XStart > 0) XStart += XStep;
+            if (YStart > 0) YStart += YStep;
+        }
+    }
+}
